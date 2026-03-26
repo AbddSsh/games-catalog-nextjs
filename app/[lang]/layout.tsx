b4445a4e5436fd1f2a84a103b082b78dda4dba78
AbddSsh <abduraimovshakh@gmail.com>
2026-03-26 10:18:14 +0500

    fix: verification tag

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -102,7 +102,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <meta name="google-site-verification" content="1Pa-5zedmxoqMHGTpMiVKO-1_e0ijaTC81Tt0RY2urg" />
         <meta
           name="impact-site-verification"
-          content="3431a947-4a6b-4ebf-8441-e920ffa6106c"
+          content="6f677166-c996-4c2d-adcb-b423bb34b92d"
         />
         <meta name="msapplication-TileColor" content="#091121" />
         <meta name="theme-color" content="#091121" />
