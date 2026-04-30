e7898a428d7fc922799311375d04a48f9a67e4b0
AbddSsh <abduraimovshakh@gmail.com>
2026-04-30 15:25:30 +0500

    Revert "chore: temporary rollback main to 0e4cf20 for prod stability"
    This reverts commit 91a6542122cf58594f078b5e7e8569639451c119.

---
+++ app/[lang]/game/[slug]/loading.tsx
@@ -0,0 +1,24 @@
+import { Skeleton } from "@/shared/ui";
+
+export default function Loading() {
+  return (
+    <div className="space-y-6 pt-[40px] max-w-[1080px] mx-auto">
+      {/* Hero skeleton */}
+      <Skeleton className="h-64 w-full rounded-[20px] md:h-80" />
+
+      {/* Section title */}
+      <Skeleton className="h-8 w-48" />
+
+      {/* Grid skeleton */}
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        {Array.from({ length: 6 }).map((_, i) => (
+          <div key={i} className="space-y-2">
+            <Skeleton className="aspect-video w-full rounded-lg" />
+            <Skeleton className="h-5 w-3/4" />
+            <Skeleton className="h-4 w-1/2" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
